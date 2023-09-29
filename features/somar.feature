efefffa7ed70ff968d4224cd7831355732622b4d
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-09-29 13:02:46 -0300

    teste com tdd

--- features/somar.feature
+++
@@ -1,5 +0,0 @@
-Feature: Somar dois números
-  Scenario: Realizar uma soma simples 01
-    Given eu tenho dois números inteiros: 5 e 7
-    When eu somo os dois números inteiros
-    Then o resultado deve ser 12
---
+++ tests/test_calculator.py
@@ -0,0 +1,31 @@
+from src.calculator import Calculator
+
+
+class Test_Calculator:
+
+    # TODOS OS TESTES DEPENDEM DA MÉTRICA QUE VOCÊ UTILIZARÁ NO SEU PROJETO
+
+    def test_add(self):
+        result = Calculator.add(2, 3)
+        assert result == 5
+    
+    def test_add2(self):
+        result = Calculator.add(2, -3)
+        assert result == -1
+
+    def test_subtracao(self):
+        result = Calculator.subtracao(2, -3)
+        assert result == 5
+
+    def test_multiplicacao(self):
+        result = Calculator.multi(2, 3)
+        assert result == 6
+
+    def test_divisao(self):
+        result = Calculator.divisao(9, 3)
+        assert result == 3
+    
+     # E SE FOR DIVISÃO POR ZERO?????????
+    # def test_divisao_por_zero(self):
+    #     result = Calculator.divisao(9, 0)
+    #     assert result == 0
